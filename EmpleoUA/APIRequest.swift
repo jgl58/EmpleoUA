0a107005c7d5d7b892700363982a23b2cf91923e
Jonay Gilabert López <jgl58@alu.ua.es>
2020-04-23 17:44:05 +0200

    Creada vista webview para mostrar practicas

--- EmpleoUA/APIRequest.swift
+++ EmpleoUA/APIRequest.swift
@@ -16,43 +16,55 @@ class APIRequest {
         let session = URLSession.shared
         let url = URL(string: self.base_url+url)!
         let task = session.dataTask(with: url, completionHandler: { data, response, error in
-            // Check the response
-            print(response)
             // Check if an error occured
             if error != nil {
                 // HERE you can manage the error
                 print(error)
                 callback(nil)
             }
-            
             do {
-                
                 let json = try JSONDecoder().decode([Tag].self, from: data!)
 //                let json = try? JSONSerialization.jsonObject(with: data!, options: [])
-
                 callback(json)
            } catch {
                print("Error during JSON serialization: \(error.localizedDescription)")
            }
-            
         })
         task.resume()
         
     }
+    static func getTag(url: String,callback: @escaping (Tag?)->Void){
+            let session = URLSession.shared
+            let url = URL(string: self.base_url+url)!
+            let task = session.dataTask(with: url, completionHandler: { data, response, error in
+                // Check if an error occured
+                if error != nil {
+                    // HERE you can manage the error
+                    print(error)
+                    callback(nil)
+                }
+                do {
+                    let json = try JSONDecoder().decode(Tag.self, from: data!)
+    //                let json = try? JSONSerialization.jsonObject(with: data!, options: [])
+                    callback(json)
+               } catch {
+                   print("Error during JSON serialization: \(error.localizedDescription)")
+               }
+            })
+            task.resume()
+            
+        }
     
     static func getActividad(class: Any, url: String,callback: @escaping (Actividad?)->Void){
         let session = URLSession.shared
         let url = URL(string: self.base_url+url)!
         let task = session.dataTask(with: url, completionHandler: { data, response, error in
-            // Check the response
-            print(response)
             // Check if an error occured
             if error != nil {
                 // HERE you can manage the error
                 print(error)
                 callback(nil)
             }
-            
             do {
                let json = try JSONDecoder().decode(Actividad.self, from: data!)
                 callback(json)
---
+++ EmpleoUA/TagDetallesViewController.swift
@@ -0,0 +1,103 @@
+//
+//  TagDetallesViewController.swift
+//  EmpleoUA
+//
+//  Created by Jonay Gilabert López on 23/04/2020.
+//  Copyright © 2020 Jonay Gilabert López. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class TagDetallesViewController: UIViewController {
+    
+    var tagID : Int?
+    private let webView = WKWebView(frame: .zero)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        APIRequest.getTag(url: "/apiCategorias/tags/"+String(tagID!)){ data in
+            if let act = data {
+                if act.name == "Prácticas y empleo"{
+                    OperationQueue.main.addOperation {
+                        self.setupButtons()
+                    }
+                }else{
+                    
+                }
+            }
+            
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func setWebView(url: URL){
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+           self.view.addSubview(self.webView)
+        // You can set constant space for Left, Right, Top and Bottom Anchors
+                            NSLayoutConstraint.activate([
+                                self.webView.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+                                self.webView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+                                self.webView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+                                self.webView.topAnchor.constraint(equalTo: self.view.topAnchor),
+                                ])
+            // For constant height use the below constraint and set your height constant and remove either top or bottom constraint
+            //self.webView.heightAnchor.constraint(equalToConstant: 200.0),
+
+            self.view.setNeedsLayout()
+            let request = URLRequest(url: url)
+            self.webView.load(request)
+    }
+    
+    func setupButtons(){
+        let frame = self.view.frame
+        
+        let myX = frame.width/4
+        var myY = frame.height/3
+        
+        let myHeight = frame.height/5
+        let myWidth = frame.width/2
+        let padding : CGFloat = 10.0
+        
+        var idColor = 0
+        
+        let button = UIButton(frame: CGRect(x: myX, y: myY, width: myWidth, height:myHeight))
+        button.tag = 1
+        button.backgroundColor = #colorLiteral(red: 0.0431372549, green: 0.3019607843, blue: 0.4196078431, alpha: 1)
+        button.setTitle("Prácticas", for: [])
+        button.titleLabel?.font = UIFont(name: "Quicksand", size: 20.0)
+        button.addTarget(self, action: #selector(pressed(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(button)
+        
+        idColor += 1
+        myY += myHeight + padding
+        
+        let buttonPortal = UIButton(frame: CGRect(x: myX, y: myY, width: myWidth, height:myHeight))
+        buttonPortal.tag = 2
+        buttonPortal.backgroundColor = #colorLiteral(red: 0.0431372549, green: 0.3019607843, blue: 0.4196078431, alpha: 1)
+        buttonPortal.setTitle("Portal de empleo", for: [])
+        buttonPortal.titleLabel?.font = UIFont(name: "Quicksand", size: 20.0)
+        buttonPortal.addTarget(self, action: #selector(pressed(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(buttonPortal)
+            
+    }
+    
+    @objc func pressed(_ sender: UIButton!) {
+        
+        if sender.tag == 1 {
+            print("Hola")
+            let url = URL(string: "https://web.ua.es/es/centro-empleo/practicas-y-empleo/practicas-en-la-ua/")!
+            setWebView(url: url)
+        }else if sender.tag == 2 {
+            let url = URL(string: "https://web.ua.es/es/centro-empleo/practicas-y-empleo/ofertas-de-empleo-para-titulados-de-la-ua/")!
+            setWebView(url: url)
+        }
+        
+    }
+
+}
